f8421d721e4e55e959c60fd3ca59f3f0759aaa9b
Muhammad Mohsin <mmohsin@rabin.cs.nmsu.edu>
2018-01-26 15:15:05 -0700

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+//******************************************************
+//CS 172
+//HelloWorld.java
+//Written by Muhammad Mohsin
+//Written Date 8/22/16
+//Print "Hello, World" in the terminal window
+//******************************************************
+
+public class HelloWorld {
+  public static void main (String[] args) {
+    //Prints "Hello, World" in the terminal Window.
+    System.out.println("Hello, World!");
+  }
+}
